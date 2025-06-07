cd3fb1e6570ebcb6194987489e832b2ad1a93a9e
Miri <Miriannyn2003@gmail.com>
2025-06-07 11:40:59 -0400

    leroy

--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def saludo(request):
-    return HttpResponse('Pasame la materia')
+    return HttpResponse('hola leroy')
 
